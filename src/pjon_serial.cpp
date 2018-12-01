7914ddbd2f1f83a5a68fc101cab03a4ed670fed5
Jaremy Creechley <creechley@gmail.com>
2018-11-30 22:31:22 -0700

    updates

--- src/pjon_serial.cpp
+++ src/pjon_serial.cpp
@@ -87,9 +87,9 @@ int main(int argc, char const *argv[]) {
   }).detach();
 
   do {
+    bus.update();
     bus.receive(PJON_RX_WAIT_TIME);
 
-    bus.update();
     if (port_rx_len.load() > 0) {
       int rx_len = port_rx_len.load();
       #ifdef PJON_SEND_BLOCKING

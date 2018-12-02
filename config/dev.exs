e8bdbc351da61217817201762d8199d9d0b23811
Jaremy Creechley <creechley@gmail.com>
2018-12-01 22:02:41 -0700

    max packet length

--- config/dev.exs
+++ config/dev.exs
@@ -5,6 +5,7 @@ config :pjon_elixir_serial, :device, System.get_env("MIX_UART") || "ttyACM0"
 config :pjon_elixir_serial, :compile_options,
     packet_size: 128,
     include_packet_id: true,
+    packet_max_length: 64,
     max_recent_ids: 4096,
     ts_response_timeout: 150000,
     pjon_send_blocking: true,
